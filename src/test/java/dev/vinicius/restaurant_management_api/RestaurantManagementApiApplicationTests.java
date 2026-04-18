e0e0f863225915ec1d45b8d6f36bf219dea5dd66
ViniCarmo <vinicius.carmo1106@gmail.com>
2026-04-18 13:53:22 -0300

    chore: Initial project setup

---
+++ src/test/java/dev/vinicius/restaurant_management_api/RestaurantManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.vinicius.restaurant_management_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantManagementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
