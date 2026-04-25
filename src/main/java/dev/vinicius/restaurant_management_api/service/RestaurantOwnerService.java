d9ce1fd9f939ce974d2b328b8550175653ba835c
ViniCarmo <vinicius.carmo1106@gmail.com>
2026-04-25 16:16:57 -0300

    feat(restaurant-owner): create service class and add createRestaurantOwner method

---
+++ src/main/java/dev/vinicius/restaurant_management_api/service/RestaurantOwnerService.java
@@ -0,0 +1,35 @@
+package dev.vinicius.restaurant_management_api.service;
+
+import dev.vinicius.restaurant_management_api.dto.RestaurantOwnerRequestDto;
+import dev.vinicius.restaurant_management_api.entities.RestaurantOwner;
+import dev.vinicius.restaurant_management_api.repository.RestaurantOwnerRepository;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
+public class RestaurantOwnerService {
+
+    private final RestaurantOwnerRepository restaurantOwnerRepository;
+
+    public RestaurantOwnerService(RestaurantOwnerRepository restaurantOwnerRepository) {
+        this.restaurantOwnerRepository = restaurantOwnerRepository;
+    }
+
+    private void createRestaurantOwner(RestaurantOwnerRequestDto restaurantOwnerRequestDto) {
+        RestaurantOwner restaurantOwner = new RestaurantOwner(
+                null,
+                restaurantOwnerRequestDto.name(),
+                restaurantOwnerRequestDto.email(),
+                restaurantOwnerRequestDto.login(),
+                restaurantOwnerRequestDto.password(),
+                LocalDateTime.now(),
+                restaurantOwnerRequestDto.address(),
+                restaurantOwnerRequestDto.restaurantName()
+        );
+
+        restaurantOwnerRepository.save(restaurantOwner);
+    }
+
+
+}
