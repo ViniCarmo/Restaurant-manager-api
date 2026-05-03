1edc581a7bd1cb09db1d559a3b59db8aa2c4c836
ViniCarmo <vinicius.carmo1106@gmail.com>
2026-05-03 17:20:59 -0300

    docs(auth): add Swagger annotations to AuthController
    Document login endpoint with @Operation and @ApiResponses:
    - POST /auth: 200, 400, 403

--- src/main/java/dev/vinicius/restaurant_management_api/controllers/AuthController.java
+++ src/main/java/dev/vinicius/restaurant_management_api/controllers/AuthController.java
@@ -4,6 +4,9 @@ import dev.vinicius.restaurant_management_api.dto.LoginRequestDto;
 import dev.vinicius.restaurant_management_api.dto.LoginResponseDto;
 import dev.vinicius.restaurant_management_api.entities.User;
 import dev.vinicius.restaurant_management_api.infra.security.service.TokenService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -26,6 +29,12 @@ public class AuthController {
         this.tokenService = tokenService;
     }
 
+    @Operation(summary = "Realiza login e retorna token JWT")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Login realizado com sucesso"),
+            @ApiResponse(responseCode = "400", description = "Dados inválidos"),
+            @ApiResponse(responseCode = "403", description = "Login ou senha incorretos")
+    })
     @PostMapping
     public ResponseEntity login(@RequestBody @Valid LoginRequestDto loginRequestDto) {
         var authToken = new UsernamePasswordAuthenticationToken(loginRequestDto.emailOrLogin(), loginRequestDto.password());
