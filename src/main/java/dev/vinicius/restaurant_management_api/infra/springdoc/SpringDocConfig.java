8ba76e3412b79471297ea27d3254617f6535d976
ViniCarmo <vinicius.carmo1106@gmail.com>
2026-05-03 16:58:10 -0300

    feat(docs): add SpringDoc OpenAPI configuration with JWT security scheme

---
+++ src/main/java/dev/vinicius/restaurant_management_api/infra/springdoc/SpringDocConfig.java
@@ -0,0 +1,23 @@
+package dev.vinicius.restaurant_management_api.infra.springdoc;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringDocConfig {
+
+    @Bean
+    public OpenAPI customOpenApi() {
+        return new OpenAPI().info(new Info()
+                        .title("Restaurant Management Api")
+                        .description("API para gestão de restaurantes e usuários")
+                        .version("v1"))
+                .components(new Components()
+                        .addSecuritySchemes("bearer-key",
+                                new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")));
+    }
+}
